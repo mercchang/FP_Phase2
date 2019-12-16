ff23719545fe56de3d8415a7b2a5ffea782e4f45
Khumor Chang <mercchang@gmail.com>
2019-12-15 21:46:32 -0500

    front end stuff, published but not working yet

--- FP_Phase2/Scripts/customSwagger.js
+++ FP_Phase2/Scripts/customSwagger.js
@@ -1,11 +1,13 @@
 ﻿$(function() {
-    $("#logo").html("&nbsp; &nbsp; TITLE");
+    $("#logo").html("&nbsp; &nbsp; Financial Portal API");
 
     $("input[name='baseUrl'], input[name='apiKey']").css("display", "none");
 
-    $("link[type='image/png']").attr("href", "/favicon.ico");
+    $("link[type='image/png']").attr("href", "Images/favicon.png");
 
     $("title").text("Financial Portal API");
+
+    $(".info_title").text("");
 
     //$("#explore").text("Refresh");
 
