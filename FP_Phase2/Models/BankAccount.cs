31359a617c9fbf01e8c07d1acd4ed91733fc92d8
Khumor Chang <mercchang@gmail.com>
2019-12-17 13:54:15 -0500

    colors & help page
    fixed scrolls and font colors for help page

--- FP_Phase2/Models/BankAccount.cs
+++ FP_Phase2/Models/BankAccount.cs
@@ -13,47 +13,46 @@ namespace FP_Phase2.Models
     public class BankAccount
     {
         /// <summary>
-        /// 
+        /// Bank Account Id
         /// </summary>
         public int Id { get; set; }
         /// <summary>
-        /// 
+        /// Household Id
         /// </summary>
         public int HouseholdId { get; set; }
         /// <summary>
-        /// 
+        /// Id of Account Owner
         /// </summary>
         public string OwnerId { get; set; }
         /// <summary>
-        /// 
+        /// Name of Account
         /// </summary>
-
         [StringLength(50, MinimumLength = 2), Required]
         public string Name { get; set; }
         /// <summary>
-        /// 
+        /// Created Date of Bank Account
         /// </summary>
         public DateTime Created { get; set; }
         /// <summary>
-        /// 
+        /// Type of account: Checking or Savings
         /// </summary>
         public AccountType AccountType { get; set; }
         /// <summary>
-        /// 
+        /// Starting balance of Account
         /// </summary>
         public float StartingBalance { get; set; }
         /// <summary>
-        /// 
+        /// Current balance of account
         /// </summary>
         public float CurrentBalance { get; set; }
         /// <summary>
-        /// 
+        /// Low balance warning for account
         /// </summary>
         public float? LowBalance { get; set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
+        ///// <summary>
+        ///// 
+        ///// </summary>
         //public virtual Household Household { get; set; }
         ///// <summary>
         ///// 
